a02530ffe1d05046b8476d7284629de0c85d575a
Omar Baltaji <omr.baltaji@gmail.com>
2023-11-28 22:55:09 +0200

    unicafe revisited, step2

--- unicafe-redux/src/main.jsx
+++ unicafe-redux/src/main.jsx
@@ -7,21 +7,21 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
+  const action = (type) => {
     store.dispatch({
-      type: 'GOOD'
+      type
     })
   }
 
   return (
     <div>
-      <button onClick={good}>good</button> 
-      <button>ok</button> 
-      <button>bad</button>
-      <button>reset stats</button>
+      <button onClick={() => action('GOOD')}>good</button> 
+      <button onClick={() => action('OK')}>ok</button> 
+      <button onClick={() => action('BAD')}>bad</button>
+      <button onClick={() => action('ZERO')}>reset stats</button>
       <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
+      <div>ok {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
     </div>
   )
 }
