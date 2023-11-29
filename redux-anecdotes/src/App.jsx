6056f7b8818732af792053e63e6e7457641ae101
Omar Baltaji <omr.baltaji@gmail.com>
2023-11-29 22:10:44 +0200

    anecdotes, step6

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -1,6 +1,7 @@
 import { useSelector, useDispatch } from 'react-redux'
 import { voteAnecdote, createAnecdote } from './reducers/anecdoteReducer'
 import AnecdoteForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
 
 const App = () => {
   const anecdotes = useSelector(state => state.sort((a, b) => b.votes - a.votes))
@@ -20,17 +21,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
-          </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={() => vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
-      )}
+      <AnecdoteList vote={vote} anecdotes={anecdotes} />
       <AnecdoteForm onSubmit={onSubmit} />
     </div>
   )
---
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,26 @@
+import PropTypes from 'prop-types';
+
+const AnecdoteList = ({ anecdotes, vote }) => {
+  return (
+    <>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
+
+AnecdoteList.propTypes = {
+  anecdotes: PropTypes.array.isRequired,
+  vote: PropTypes.func.isRequired,
+}
+
+export default AnecdoteList;
