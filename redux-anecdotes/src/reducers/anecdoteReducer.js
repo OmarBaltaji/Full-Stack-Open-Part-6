e54679aa68a938738bf7014e17321eb480c7b09a
Omar Baltaji <omr.baltaji@gmail.com>
2023-11-29 21:50:58 +0200

    anecdotes, step1

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -20,10 +20,23 @@ const asObject = (anecdote) => {
 const initialState = anecdotesAtStart.map(asObject)
 
 const reducer = (state = initialState, action) => {
-  console.log('state now: ', state)
-  console.log('action', action)
-
-  return state
+  switch(action.type) {
+    case 'VOTE':
+      return state.map(anecdote => {
+        if (anecdote.id === action.payload.id) {
+          return { ...anecdote, votes: anecdote.votes + 1 };
+        }
+        return anecdote;
+      })
+    default: 
+      return state;
+  }
 }
+
+export const voteAnecdote = (id) => ({
+  type: 'VOTE',
+  payload: { id  }
+})
+
 
 export default reducer
